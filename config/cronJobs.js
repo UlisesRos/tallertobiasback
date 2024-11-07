baebfd5da6ef3684728e1294d1b2396d992c8d3f
Ulises Ros <ulisesros70@gmail.com>
2024-11-07 18:56:12 -0300

    finalizacion twilio

--- config/cronJobs.js
+++ config/cronJobs.js
@@ -11,7 +11,7 @@ const client = twilio(accountSid, authToken);
 
 const iniciarCronJobs = () => {
 
-    cron.schedule('0 11 * * *', async () => {
+    cron.schedule('0 10 * * *', async () => {
         try {
             console.log('Iniciando tarea de actualizacion de proximoServicio');
             const registros = await Cliente.findAll({
@@ -32,10 +32,12 @@ const iniciarCronJobs = () => {
                 if(registro.Servicios[0].proximoServicio === 0){
                     registro.Servicios[0].proximoServicio = 'Realizar Proximo Servicio'
 
+                    console.log(registro.Servicios[0].descripcionProximoServicio)
+
                     client.messages.create({
                         from: 'whatsapp:+14155238886',
                         to: 'whatsapp:+5493413632945',
-                        body: `El cliente ${registro.nombre} necesita un nuevo Servicio. Comunicate a su Celular para contactar con el: ${registro.telefono}`
+                        body: `El cliente ${registro.nombre} necesita un nuevo Servicio. Servicio a realizar: ${registro.Servicios[0].descripcionProximoServicio}. Comunicate a su Celular para contactar con el: ${registro.telefono}`
                     })
                     .then((message) => console.log(`Mensaje enviado con ID: ${message.sid}`))
                     .catch((error) => console.error('Error al enviar el mensaje:', error))
