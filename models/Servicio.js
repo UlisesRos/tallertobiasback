f048691fa7ae01948f4396aae3fba4e3748e0ece
Ulises Ros <ulisesros70@gmail.com>
2024-12-12 22:58:25 -0300

    cambios

--- models/Servicio.js
+++ models/Servicio.js
@@ -20,6 +20,16 @@ const Servicio = sequelize.define('Servicio', {
         type: DataTypes.STRING,
         allowNull: false
     },
+    pago: {
+        type: DataTypes.FLOAT,
+        allowNull: true,
+        defaultValue: 0
+    },
+    deuda: {
+        type: DataTypes.FLOAT,
+        allowNull: true,
+        defaultValue: 0
+    },
     proximoServicio: {
         type: DataTypes.STRING,
         allowNull: true
